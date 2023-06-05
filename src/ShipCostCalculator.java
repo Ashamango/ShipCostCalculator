ff4d09c20b04b970c2f78b26d05e5becb6206202
Ashamango <ashaanupdas@gmail.com>
2023-06-05 18:05:25 -0400

    If the item is less than $100, there is a 2% shipping fee added on, otherwise if the item is $100 or more, shipping is free. It displays the shipping and total cost.

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,8 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner; // before the class at top of the file
 public class ShipCostCalculator {
     public static void main(String[] args) {
-  
+        Scanner in = new Scanner(System.in);
+
+        // Declare variables
+        double shipping; // num shipping
+        double totalCost; // num totalCost
+
+        // Prompt and input
+        System.out.print("Enter the price of the item:  "); // output "Enter the price of the item
+        double price = in.nextDouble(); // num price
+
+        if(price >= 100)
+        {
+            shipping = 0;
+            totalCost = price + shipping;
+            in.nextLine();
+            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars.");
+        }
+        else
+        {
+            shipping = 0.02 * price;
+            totalCost = shipping + price;
+            in.nextLine();
+            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars.");
         }
     }
 }
