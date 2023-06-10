35b5094f93143771cf0025ec7a314170008088ad
Ashamango <ashaanupdas@gmail.com>
2023-06-10 10:34:17 -0400

    If the item is less than $100, there is a 2% shipping fee added on, otherwise if the item is $100 or more, shipping is free. It displays the shipping and total cost.

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // before the class at top of the file
-public class ShipCostCalculator {
-    public static void main(String[] args) {
+public class ShipCostCalculator { // class ShipCostCalculator
+    public static void main(String[] args) { // main
         Scanner in = new Scanner(System.in);
 
         // Declare variables
@@ -8,22 +8,21 @@ public class ShipCostCalculator {
         double totalCost; // num totalCost
 
         // Prompt and input
-        System.out.print("Enter the price of the item:  "); // output "Enter the price of the item
+        System.out.print("Enter the price of the item:  "); // output "Enter the price of the item: "
         double price = in.nextDouble(); // num price
 
-        if(price >= 100)
+        if (price >= 100) // if price >= 100 then
         {
-            shipping = 0;
-            totalCost = price + shipping;
+            shipping = 0; // shipping = 0
+            totalCost = price + shipping; // totalCost = price + shipping
             in.nextLine();
-            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars.");
-        }
-        else
+            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars."); // output "The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars."
+        } else // else
         {
-            shipping = 0.02 * price;
-            totalCost = shipping + price;
+            shipping = 0.02 * price; // shipping = 0.02 * price
+            totalCost = shipping + price; // totalCost = shipping + price
             in.nextLine();
-            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars.");
-        }
-    }
-}
+            System.out.print("The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars."); // output "The shipping cost is " + shipping + " dollars and the total cost is " + totalCost + " dollars."
+        } // endif
+    } // return
+} // end class
